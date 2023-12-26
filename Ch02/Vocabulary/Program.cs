b8f95d7de65711a2484400ccea92513010cc72e7
Wei Ding <ding_web@yahoo.co.jp>
2023-12-26 01:18:35 -0800

    ch02 number of types

--- Ch02/Vocabulary/Program.cs
+++ Ch02/Vocabulary/Program.cs
@@ -1,17 +1,28 @@
-﻿namespace Vocabulary;
+﻿using System.Reflection;
+
+namespace Vocabulary;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Ch02!");
-        //#error version
-
-        #region dsp
-        Console.WriteLine("Line 1");
-        WriteLine("Line 2");
-        #endregion
-
         WriteLine($"Computer named {Env.MachineName} says \"Yes\".");
+
+        Assembly? myApp = Assembly.GetEntryAssembly();
+        if (myApp is null)
+            return;
+        foreach(var name in myApp.GetReferencedAssemblies()){
+            Assembly a = Assembly.Load(name);
+            int methodCount = 0;
+            foreach(var t in a.DefinedTypes)
+                methodCount += t.GetMembers().Length;
+
+            WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+                arg0:a.DefinedTypes.Count(),
+                arg1:methodCount,
+                arg2:name.Name
+            );
+        }
+
     }
 }
