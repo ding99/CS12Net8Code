fd61ab9a4404d1db5a99d0dcbdf0617263f3e6cc
Wei Ding <ding_web@yahoo.co.jp>
2024-01-06 18:22:48 -0800

    statically import

---
+++ Ch02/Formatting/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+int numberOfApples = 12;
+decimal price = 0.45M;
+WriteLine(
+    format:"{0} apples cost {1:C}",
+    arg0:numberOfApples,
+    arg1:price * numberOfApples);
+string formatted = string.Format(
+    format:"{0} apples cost {1:C}",
+    arg0:numberOfApples,
+    arg1: price * numberOfApples);
+WriteLine(formatted);
