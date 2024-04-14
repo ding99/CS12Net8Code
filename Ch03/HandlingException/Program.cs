131b79934765dd2cbead4819ba525ea6f38f2fd8
Wei Ding <ding_web@yahoo.co.jp>
2024-04-14 03:48:34 -0700

    ch03 unchecked

--- Ch03/HandlingException/Program.cs
+++ Ch03/HandlingException/Program.cs
@@ -7,13 +7,27 @@ System.Console.WriteLine($"After incrementing: {x}");
 x++;
 System.Console.WriteLine($"After incrementing: {x}");
 
-checked {
-x = int.MaxValue - 1;
-System.Console.WriteLine($"Initial value: {x}");
-x++;
-System.Console.WriteLine($"After incrementing: {x}");
-x++;
-System.Console.WriteLine($"After incrementing: {x}");
-x++;
-System.Console.WriteLine($"After incrementing: {x}");
+try {
+    checked{
+        x = int.MaxValue - 1;
+        System.Console.WriteLine($"Initial value: {x}");
+        x++;
+        System.Console.WriteLine($"After incrementing: {x}");
+        x++;
+        System.Console.WriteLine($"After incrementing: {x}");
+        x++;
+        System.Console.WriteLine($"After incrementing: {x}");
+    }
+}
+catch(OverflowException){
+    System.Console.WriteLine("The code overflowed but I caught the exception.");
+}
+
+unchecked {
+int y = int.MaxValue + 1;
+System.Console.WriteLine($"Initial value: {y}");
+y--;
+System.Console.WriteLine($"After decrementing: {y}");
+y--;
+System.Console.WriteLine($"After decrementing: {y}");
 }
