1d625e9525a6451a40f54d4e9b16002991ca8711
Wei Ding <ding_web@yahoo.co.jp>
2024-04-14 03:11:18 -0700

    ch03 checked

---
+++ Ch03/HandlingException/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = int.MaxValue - 1;
+System.Console.WriteLine($"Initial value: {x}");
+x++;
+System.Console.WriteLine($"After incrementing: {x}");
+x++;
+System.Console.WriteLine($"After incrementing: {x}");
+x++;
+System.Console.WriteLine($"After incrementing: {x}");
+
+checked {
+x = int.MaxValue - 1;
+System.Console.WriteLine($"Initial value: {x}");
+x++;
+System.Console.WriteLine($"After incrementing: {x}");
+x++;
+System.Console.WriteLine($"After incrementing: {x}");
+x++;
+System.Console.WriteLine($"After incrementing: {x}");
+}
