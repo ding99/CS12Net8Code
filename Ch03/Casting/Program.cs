721f99baebdc381423607ae95190e7c9eac8c9b2
Wei Ding <ding_web@yahoo.co.jp>
2024-04-14 01:09:57 -0700

    ch03 cast

---
+++ Ch03/Casting/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+System.Console.WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i >= -8; i--){
+    System.Console.WriteLine("{0,12} {0,34:B32}", i);
+}
+System.Console.WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
