f2ef10323e6bf631106d53c43b675fdcbc7368bc
Wei Ding <ding_web@yahoo.co.jp>
2024-04-14 02:31:13 -0700

    ch03 tryparse

--- Ch03/Casting/Program.cs
+++ Ch03/Casting/Program.cs
@@ -1,7 +1,23 @@
-﻿System.Console.WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+﻿using static System.Convert;
+using System.Globalization;
+
+System.Console.WriteLine("{0,12} {1,34}", "Decimal", "Binary");
 System.Console.WriteLine("{0,12} {0,34:B32}", int.MaxValue);
 for (int i = 4; i >= -4; i--){
     System.Console.WriteLine("{0,12} {0,34:B32}", i);
 }
 System.Console.WriteLine("{0,12} {0,34:B32}", int.MinValue);
 
+Console.ForegroundColor = ConsoleColor.Cyan;
+double g = 9.8;
+int h = ToInt32(g);
+System.Console.WriteLine($"g is {g}, h is {h}");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+string dt1 = "5 June 1999";
+string dt2 = "5a June 19929";
+System.Console.WriteLine($"{dt1} : {DateTime.TryParse(dt1, out DateTime d1)} [{d1}]");
+System.Console.WriteLine($"{dt2} : {DateTime.TryParse(dt2, out DateTime d2)} [{d2}]");
+
+Console.ResetColor();
