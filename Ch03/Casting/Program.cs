24cb8eb27587f35e2f693fe84ff28256d2658c0e
Wei Ding <ding_web@yahoo.co.jp>
2024-04-14 01:11:46 -0700

    ch03 change range

--- Ch03/Casting/Program.cs
+++ Ch03/Casting/Program.cs
@@ -1,6 +1,6 @@
 ﻿System.Console.WriteLine("{0,12} {1,34}", "Decimal", "Binary");
 System.Console.WriteLine("{0,12} {0,34:B32}", int.MaxValue);
-for (int i = 8; i >= -8; i--){
+for (int i = 4; i >= -4; i--){
     System.Console.WriteLine("{0,12} {0,34:B32}", i);
 }
 System.Console.WriteLine("{0,12} {0,34:B32}", int.MinValue);
