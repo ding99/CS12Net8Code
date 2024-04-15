16dffcfb0ef4f44cbb3353b3ca8e84a8c4abb113
Wei Ding <ding_web@yahoo.co.jp>
2024-04-15 01:26:02 -0700

    ch04 times table

--- Ch04/TopLevel/Program.Functions.cs
+++ Ch04/TopLevel/Program.Functions.cs
@@ -1,6 +1,5 @@
 ﻿using static System.Console;
 
-
 partial class Program
 {
     static void WhatsMyNamespace() {
---
+++ Ch04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,13 @@
+﻿partial class Program
+{
+
+    static void TimesTable(byte number, byte size=12){
+        Console.WriteLine($"This is the {number} times table with {size} rows:");
+        System.Console.WriteLine();
+        for(int row = 1; row <=size; row++){
+            System.Console.WriteLine($"{row} x {number} = {row*number}");
+        }
+        System.Console.WriteLine();
+    }
+
+}
