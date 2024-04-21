5a68da134057718753b31352f219dc3d97509f38
Wei Ding <ding_web@yahoo.co.jp>
2024-04-21 16:34:14 -0700

    ch04 function returns value

--- Ch04/WritingFunctions/Program.Functions.cs
+++ Ch04/WritingFunctions/Program.Functions.cs
@@ -1,4 +1,6 @@
-﻿partial class Program
+﻿using System.Globalization;
+
+partial class Program
 {
 
     static void TimesTable(byte number, byte size=12){
@@ -8,6 +10,27 @@
             System.Console.WriteLine($"{row} x {number} = {row*number}");
         }
         System.Console.WriteLine();
+    }
+
+    static void ConfigureConsole(string culture = "en-US", bool useComputerCulture = false){
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useComputerCulture){
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        System.Console.WriteLine($"CurrentCulture:{CultureInfo.CurrentCulture.DisplayName}");
+    }
+
+    static decimal CalculateTax(decimal amount, string regionCode){
+        decimal rate = regionCode switch{
+            "CH" => .08M, //Switzerland
+            "DK" or "NO" =>.25M, //Denmark, Norway
+            "GB" or "FR" => .2M, //UK, France
+            "HU" => .27M, //Hungary
+            "OR" or "AK" or "MT" => .0M, //Oregon, Alaska, Montana
+            "ND" or "WI" or "ME" or "VA" => .05M,
+            _ => .06M // Most other states
+        };
+        return amount * rate; 
     }
 
 }
--- Ch04/WritingFunctions/Program.cs
+++ Ch04/WritingFunctions/Program.cs
@@ -1,1 +1,5 @@
-﻿TimesTable(7, 15);
+﻿//TimesTable(7, 15);
+
+ConfigureConsole();
+decimal taxToPay = CalculateTax(149, "FR");
+System.Console.WriteLine($"You much pay {taxToPay} in tax.");
