016b2e6a68c7da7a79b8cbfdc6fc31d5fff2c46f
Wei Ding <ding_web@yahoo.co.jp>
2024-06-09 13:58:50 -0700

    ch05 field values

--- Ch05/PacktLibraryNetStandard2/Person.cs
+++ Ch05/PacktLibraryNetStandard2/Person.cs
@@ -1,5 +1,6 @@
 namespace Packt.Shared;
 
-public class Person{
-
+public class Person {
+    public string? Name;
+    public DateTimeOffset Born;
 }
--- Ch05/PeopleApp/Program.cs
+++ Ch05/PeopleApp/Program.cs
@@ -3,3 +3,8 @@
 ConfigureConsole();
 Person bob = new();
 WriteLine(bob);
+
+bob.Name = "Bob Smith";
+bob.Born = new DateTimeOffset(
+    year: 1965, month: 12, day: 22, hour: 14, minute: 28, second: 0, offset: TimeSpan.FromHours(-5));
+WriteLine(format: "{0} was born on {1:D} ({1:d}).", bob.Name, bob.Born);
