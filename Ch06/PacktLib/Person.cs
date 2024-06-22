3aed7f79b9ec1d163069a7fa37c5145d5a54a8e8
Wei Ding <ding_web@yahoo.co.jp>
2024-06-22 16:55:03 -0700

    ch06 static methods and instance methods

--- Ch06/PacktLib/Person.cs
+++ Ch06/PacktLib/Person.cs
@@ -2,20 +2,68 @@
 
 public class Person
 {
-#region Properties
+    #region Properties
     public string? Name { get; set; }
     public DateTimeOffset Born{get; set; }
-    public List<Person> Children = new();
-#endregion
+    public List<Person> Children = [];
+    public List<Person> Spouses = [];
+    public bool Married => Spouses.Count > 0;
+    #endregion
 
-#region Methods
-public void WriteToConsole(){
-    WriteLine($"{Name} was born on a {Born:dddd}.");
-}
-public void WriteChildrenToConsole(){
-    string term = Children.Count == 1 ? "child" : "children";
-    WriteLine($"{Name} has {Children.Count} {term}.");
-}
-#endregion
+    #region Methods
+    public void WriteToConsole(){
+        WriteLine($"{Name} was born on a {Born:dddd}.");
+    }
+    public void WriteChildrenToConsole(){
+        string term = Children.Count == 1 ? "child" : "children";
+        WriteLine($"{Name} has {Children.Count} {term}.");
+    }
+
+    public static void Marry(Person p1, Person p2){
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+        if(p1.Spouses.Contains(p2) || p2.Spouses.Contains(p1)){
+            throw new ArgumentException(
+                string.Format("{0} is already married to {1}.",
+                p1.Name, p2.Name));
+        }
+        p1.Spouses.Add(p2);
+        p2.Spouses.Add(p1);
+    }
+    public void Marry(Person partner){
+        Marry(this, partner);
+    }
+    public void OutputSpouses(){
+        if(Married){
+            string term = Spouses.Count == 1 ? "person" : "people";
+            WriteLine($"{Name} is married to {Spouses.Count} {term}");
+            foreach(Person spouse in Spouses){
+                WriteLine($" {spouse.Name}");
+            }
+        } else{
+            WriteLine($"{Name} is singleton.");
+        }
+    }
+
+    public static Person Procreate(Person p1, Person p2){
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+        if(!p1.Spouses.Contains(p2) && p2.Spouses.Contains(p1)){
+            throw new ArgumentException(string.Format("{0] must be married to {1} to procreatd with them.}",
+             p1.Name, p2.Name));
+        }
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}",
+            Born = DateTimeOffset.Now
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+    public Person ProcreateWith(Person partner){
+        return Procreate(this, partner);
+    }
+    #endregion
 
 }
--- Ch06/PeopleApp/Program.cs
+++ Ch06/PeopleApp/Program.cs
@@ -1,6 +1,33 @@
 ﻿using Packt.Shared;
+
+ForegroundColor = ConsoleColor.Green;
 Person harry = new() {
     Name = "Harry",
     Born = new(2002, 3, 25, 0, 0, 0, TimeSpan.Zero)
 };
 harry.WriteToConsole();
+
+ForegroundColor = ConsoleColor.Yellow;
+Person lamech = new() { Name = "Lamech" };
+Person adah = new(){Name="Adah"};
+Person zillah = new() { Name="Zillah"};
+lamech.Marry(adah);
+Person.Marry(lamech, zillah);
+lamech.OutputSpouses();
+adah.OutputSpouses();
+zillah.OutputSpouses();
+Person baby1 = lamech.ProcreateWith(adah);
+baby1.Name = "Jabal";
+WriteLine($"{baby1.Name} was born on {baby1.Born}");
+Person baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tubalcain";
+adah.WriteChildrenToConsole();
+zillah.WriteChildrenToConsole();
+lamech.WriteChildrenToConsole();
+for(int i = 0; i<lamech.Children.Count;i++){
+    WriteLine(" {0}'s child #{1} is named \"{2}\".",
+    lamech.Name, i,
+    lamech.Children[i].Name);
+}
+
+ResetColor();
